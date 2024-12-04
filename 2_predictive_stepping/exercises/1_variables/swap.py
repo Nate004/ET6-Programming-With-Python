215e37dfce3a501aec94bc5ab612e2625320f89a
Nate004 <natebabs04@gmail.com>
2024-12-04 15:33:14 +0100

    completed assertion exercise for swap.py

--- 2_predictive_stepping/exercises/1_variables/swap.py
+++ 2_predictive_stepping/exercises/1_variables/swap.py
@@ -10,14 +10,14 @@ Fill in the blanks to pass the assertions.
 a = 2
 b = 1
 
-assert a == _, "a's initial value"
-assert b == _, "b's initial value"
+assert a == int(input()), "a's initial value"
+assert b == int(input()), "b's initial value"
 
 temp = a
-assert temp == _, "temp's final value"
+assert temp == int(input()), "temp's final value"
 
 a = b
-assert a == _, "a's final value"
+assert a == int(input()), "a's final value"
 
 b = temp
-assert b == _, "b's final value"
+assert b == int(input()), "b's final value"
