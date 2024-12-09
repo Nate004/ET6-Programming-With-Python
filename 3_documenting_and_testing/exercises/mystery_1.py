a04a4144c16559db624a823f972ea9142da3dea0
Nate004 <natebabs04@gmail.com>
2024-12-09 10:09:16 +0100

    wrote docstring and ran 2 tests on mystery_1

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,4 +1,12 @@
 def mystery_1(a):
+    """" This sorts a list of elements in ascending order
+    
+    Parameters:
+        a: is the input parameter of List type data
+        b: an integer that stores the length of the list a; this determines the number of iterations for sorting
+        c: an integer and a loop variable which runs from 0 to b-1 (range)
+        d: an integer  which runs from 0 to b-c-1 to compare adjacent elements and swap in case of wrong order
+    """
     b = len(a)
     for c in range(b):
         for d in range(0, b - c - 1):
--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -4,5 +4,8 @@ from ..mystery_1 import mystery_1
 class TestMystery1(unittest.TestCase):
     """ Test the Mystery_1 function """
     def test_0(self):
-        """It should evaluate 0 to []"""
-        self.assertEqual(mystery_1(0))
+        """Should test that the list is sorted and remains unchanged"""
+        self.assertEqual(mystery_1([0,1,3,4,5,6]),[0,1,3,4,5,6])
+    def test_1(self):
+        """Should test for reversed sorted list"""
+        self.assertEqual(mystery_1([6,5,4,3,2,1,0]),[0,1,2,3,4,5,6])
