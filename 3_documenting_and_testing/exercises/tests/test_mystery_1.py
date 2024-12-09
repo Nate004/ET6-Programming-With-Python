bc15260c4ab1cd014a688d37ddd86443c66e88c8
Nate004 <natebabs04@gmail.com>
2024-12-09 08:40:47 +0100

    updated mystery_1 test

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -1,5 +1,8 @@
 import unittest
 
-
+from ..mystery_1 import mystery_1
 class TestMystery1(unittest.TestCase):
-    """ """
+    """ Test the Mystery_1 function """
+    def test_0(self):
+        """It should evaluate 0 to []"""
+        self.assertEqual(mystery_1(0))
