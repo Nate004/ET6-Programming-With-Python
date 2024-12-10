e47ec19878b783a148a2d7936b300a1baa611ce3
Nate004 <natebabs04@gmail.com>
2024-12-10 08:52:52 +0100

    updates

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -9,3 +9,6 @@ class TestMystery1(unittest.TestCase):
     def test_1(self):
         """Should test for reversed sorted list"""
         self.assertEqual(sort_bubble([6,5,4,3,2,1,0]),[0,1,2,3,4,5,6])
+    def test_2(self):
+        """Should sort list in correct ascending order"""
+        self.assertEqual(sort_bubble([0,2,1,3,6,5,4]),[0,1,2,3,4,5,6])
