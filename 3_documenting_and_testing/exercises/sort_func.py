d0b17e31412de94b6524e21a9f4672b0bddc4874
Nate004 <natebabs04@gmail.com>
2024-12-22 14:00:10 +0100

    wrote doctest

--- 3_documenting_and_testing/exercises/sort_func.py
+++ 3_documenting_and_testing/exercises/sort_func.py
@@ -1,7 +1,12 @@
 def sort_func(a, b=None):
     """This function sorts a list of numbers in ascending order
     returns a list b
+    Examples:
+    >>> sort_func([1, 2, 4, 3])
+    [1, 2, 3, 4]
     
+    >>> sort_func([1.0, 3.2, 1.5, 2.9, 0.5])
+    [0.5, 1.0, 1.5, 2.9, 3.2]
     
     """
     if b is None:
